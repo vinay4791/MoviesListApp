aba8fa0d206069d8fdd5466ac1b0403739286a1c
Vinay Mathew John <vinay.john@litmus7.com>
2020-05-20 22:42:00 +0530

    Added Viewmodel and attached to Fragment

--- app/src/main/java/vinay/com/movieslistapp/view/ListFragment.kt
+++ app/src/main/java/vinay/com/movieslistapp/view/ListFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_list.*
 
 import vinay.com.movieslistapp.R
+import vinay.com.movieslistapp.viewmodel.ListViewModel
 
 class ListFragment : Fragment() {
+
+    private lateinit var listViewModel: ListViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+    }
+
+    fun initialize() {
+        listViewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
     }
 }
---
+++ app/src/main/java/vinay/com/movieslistapp/viewmodel/ListViewModel.kt
@@ -0,0 +1,8 @@
+package vinay.com.movieslistapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ListViewModel(application: Application) : AndroidViewModel(application) {
+
+}
