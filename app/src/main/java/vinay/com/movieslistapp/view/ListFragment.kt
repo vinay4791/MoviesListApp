0a3d6e91617e2ea230a888b3bc072a3afd4d03c9
Vinay Mathew John <vinay.john@litmus7.com>
2020-05-20 21:08:39 +0530

    Created Movie List and Details Screens.

---
+++ app/src/main/java/vinay/com/movieslistapp/view/ListFragment.kt
@@ -0,0 +1,18 @@
+package vinay.com.movieslistapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import vinay.com.movieslistapp.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+}
---
+++ app/src/main/java/vinay/com/movieslistapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package vinay.com.movieslistapp.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import vinay.com.movieslistapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
