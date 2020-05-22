38041e01769c15a875c7b354cba9cd8892af51a8
Vinay Mathew John <vinay.john@litmus7.com>
2020-05-22 09:19:23 +0530

    Implemented basic detail screen

--- app/src/main/java/vinay/com/movieslistapp/view/DetailFragment.kt
+++ app/src/main/java/vinay/com/movieslistapp/view/DetailFragment.kt
@@ -5,16 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_detail.*
 import kotlinx.android.synthetic.main.fragment_list.*
 
 import vinay.com.movieslistapp.R
+import vinay.com.movieslistapp.databinding.FragmentDetailBinding
+import vinay.com.movieslistapp.model.Results
 
 class DetailFragment : Fragment() {
 
+    var movie: Results? = null
+    private lateinit var dataBinding: FragmentDetailBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            movie = DetailFragmentArgs.fromBundle(it).movieDetail
+        }
+
+        /*animal?.imageUrl?.let {
+            setupBackgroundColor(it)
+        }*/
+
+        dataBinding.movie = movie
     }
 }
