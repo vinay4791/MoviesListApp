8c81c862e66c9373398b16d4b8f4da6d46adaee4
Vinay Mathew John <vinay.john@litmus7.com>
2020-05-26 10:08:31 +0530

    Removed unwanted Files

--- app/src/main/java/vinay/com/movieslistapp/model/MoviesBoundaryCallback.kt
+++ app/src/main/java/vinay/com/movieslistapp/model/MoviesBoundaryCallback.kt
@@ -9,7 +9,6 @@ import io.reactivex.schedulers.Schedulers
 import vinay.com.movieslistapp.util.Constants.Companion.API_KEY
 import vinay.com.movieslistapp.util.Constants.Companion.DEFAULT_LANGUAGE
 import vinay.com.movieslistapp.util.Constants.Companion.INITIAL_PAGE
-import vinay.com.movieslistapp.util.PagingRequestHelper
 import vinay.com.movieslistapp.util.State
 import vinay.com.newsapidemoapp.db.MovieDb
 import java.util.concurrent.Executors
@@ -19,7 +18,6 @@ class MoviesBoundaryCallback(private val db: MovieDb,
                              val compositeDisposable: CompositeDisposable) : PagedList.BoundaryCallback<Results>() {
 
     private val executor = Executors.newSingleThreadExecutor()
-    private val helper = PagingRequestHelper(executor)
     private var totalPagesCount = 0;
     private var currentPageCount = 1;
     var state: MutableLiveData<State> = MutableLiveData()
@@ -27,54 +25,47 @@ class MoviesBoundaryCallback(private val db: MovieDb,
     override fun onZeroItemsLoaded() {
         super.onZeroItemsLoaded()
         //1
-        helper.runIfNotRunning(PagingRequestHelper.RequestType.INITIAL) { helperCallback ->
-            updateState(State.LOADING)
-            compositeDisposable.add(
-                    apiService.getMovies(API_KEY, DEFAULT_LANGUAGE, INITIAL_PAGE)
-                            .subscribeOn(Schedulers.newThread())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(
-                                    { response ->
-                                        totalPagesCount = response.total_pages.toInt()
-                                        executor.execute {
-                                            db.moviesDao().insert(response.results ?: listOf())
-                                            helperCallback.recordSuccess()
-                                        }
-                                    },
-                                    {
-                                        Log.e("MoviesBoundaryCallback", "Failed to load data!")
-                                        helperCallback.recordFailure(it)
-                                        updateState(State.ERROR)
+        updateState(State.LOADING)
+        compositeDisposable.add(
+                apiService.getMovies(API_KEY, DEFAULT_LANGUAGE, INITIAL_PAGE)
+                        .subscribeOn(Schedulers.newThread())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                { response ->
+                                    totalPagesCount = response.total_pages.toInt()
+                                    executor.execute {
+                                        db.moviesDao().insert(response.results ?: listOf())
                                     }
-                            )
-            )
-        }
+                                },
+                                {
+                                    Log.e("MoviesBoundaryCallback", "Failed to load data!")
+                                    updateState(State.ERROR)
+                                }
+                        )
+        )
     }
+
 
     override fun onItemAtEndLoaded(itemAtEnd: Results) {
         super.onItemAtEndLoaded(itemAtEnd)
         updateState(State.LOADING)
         if (totalPagesCount >= currentPageCount) {
-            helper.runIfNotRunning(PagingRequestHelper.RequestType.AFTER) { helperCallback ->
-                compositeDisposable.add(
-                        apiService.getMovies(API_KEY, DEFAULT_LANGUAGE, currentPageCount++)
-                                .subscribeOn(Schedulers.newThread())
-                                .observeOn(AndroidSchedulers.mainThread())
-                                .subscribe(
-                                        { response ->
-                                            executor.execute {
-                                                db.moviesDao().insert(response.results ?: listOf())
-                                                helperCallback.recordSuccess()
-                                            }
-                                        },
-                                        {
-                                            Log.e("MoviesBoundaryCallback", "Failed to load data!")
-                                            helperCallback.recordFailure(it)
-                                            updateState(State.ERROR)
+            compositeDisposable.add(
+                    apiService.getMovies(API_KEY, DEFAULT_LANGUAGE, currentPageCount++)
+                            .subscribeOn(Schedulers.newThread())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(
+                                    { response ->
+                                        executor.execute {
+                                            db.moviesDao().insert(response.results ?: listOf())
                                         }
-                                )
-                )
-            }
+                                    },
+                                    {
+                                        Log.e("MoviesBoundaryCallback", "Failed to load data!")
+                                        updateState(State.ERROR)
+                                    }
+                            )
+            )
         } else {
             updateState(State.DONE)
         }
